0aeecad7a78500cd116254842cea73ccc177b629
Vavaaaahhhh <gelly.valentin@gmail.com>
2022-10-25 10:51:42 +0200

    début page utilisateurAdministrateur

--- views/UtilisateurAdministrateur.php
+++ views/UtilisateurAdministrateur.php
@@ -5,7 +5,18 @@
         <link rel="stylesheet" href="../assets/css/accueilAdmin.css">
     </head>
     <body>
-    <p>coucou</p>
+    <nav>
+            <img src="../assets/img/logo.png" alt="logo">
+            <ul>
+                <li class="hover"><a href="#" >Accueil</a></li>
+                <li><a href="#">Planning</a></li>
+                <li><a href="#">Prise de rendez-vous</a></li>
+                <li><a href="#">Stock</a></li>
+                <li><a href="#">Tarifs</a></li>
+                <li><a href="#">Clients</a></li>
+                <li><a href="#">Deconnexion</a></li>
+            </ul>
+        </nav>
 
     </body>
 </html>
