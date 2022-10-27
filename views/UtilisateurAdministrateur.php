b9b92ced8224613a0738148c3537721ceadecf22
Vavaaaahhhh <gelly.valentin@gmail.com>
2022-10-27 09:48:35 +0200

    page adminUtilisateur fait

--- views/UtilisateurAdministrateur.php
+++ views/UtilisateurAdministrateur.php
@@ -3,9 +3,55 @@
     <head>
         <link rel="stylesheet" href="../assets/css/style.css">
         <link rel="stylesheet" href="../assets/css/accueilAdmin.css">
+        <link rel="stylesheet" href="../assets/css/utilisateurAdministrateur.css">
     </head>
     <body>
-    <p>coucou</p>
+    <nav>
+        <img src="../assets/img/logo.png" alt="logo">
+        <ul>
+            <li><a href="#" >Accueil</a></li>
+            <li class="hover"><a href="#">Utilisateur</a></li>
+            <li><a href="#">Tarification</a></li>
+            <li><a href="#">Stock</a></li>
+            <li><a href="#">Intervention</a></li>
+            <li><a href="#">Deconnexion</a></li>
+        </ul>
+    </nav>
+   <main>
+       <aside>
+           <ul>
+               <li class="hoverli"><a href="#">Tableau de bord</a></li>
+               <li><a href="#">Client</a></li>
+               <li><a href="#">Garage</a></li>
+           </ul>
+       </aside>
+       <section class="main">
+           <a href="#">
+               <h1>Ajouter un client</h1>
+               <img src="/assets/img/add-clients.png" alt="">
+           </a>
+           <a href="#">
+               <h1>modifier un client</h1>
+               <img src="/assets/img/add-clients.png" alt="">
+           </a>
+           <a href="#">
+               <h1>Supprimer un client</h1>
+               <img src="/assets/img/add-clients.png" alt="">
+           </a>
+           <a href="#">
+               <h1>Ajouter un rôle à un employé</h1>
+               <img src="/assets/img/add-clients.png" alt="">
+           </a>
+           <a href="#">
+               <h1>Modifier les rôle d'un employé</h1>
+               <img src="/assets/img/add-clients.png" alt="">
+           </a>
+           <a href="#">
+               <h1>Supprimer un rôle à un employé </h1>
+               <img src="/assets/img/add-clients.png" alt="">
+           </a>
+       </section>
+   </main>
 
     </body>
 </html>
