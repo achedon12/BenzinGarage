3355c781e97043a0e567d500f3e743d0a8e8a2a4
Azoxlpb <azox.rekt@gmail.com>
2022-11-18 10:21:21 +0100

    véhicule class

--- assets/php/class/Vehicle.php
+++ assets/php/class/Vehicle.php
@@ -2,21 +2,60 @@
 
 class Vehicle{
 
-    private int $id;
-
     private string $numberPlate;
-
+    private string $noSerie;
+    private string $dateMiseEnCirculation;
+    private int $numModele;
     private Client $client;
 
-    public function __construct(int $id, string $numberPlate, Client $client){
-        $this->id = $id;
+    /**
+     * @param string $numberPlate
+     * @param string $noSerie
+     * @param string $dateMiseEnCirculation
+     * @param int $numModele
+     * @param Client $client
+     */
+    public function __construct(string $numberPlate, string $noSerie, string $dateMiseEnCirculation, int $numModele, Client $client)
+    {
         $this->numberPlate = $numberPlate;
+        $this->noSerie = $noSerie;
+        $this->dateMiseEnCirculation = $dateMiseEnCirculation;
+        $this->numModele = $numModele;
         $this->client = $client;
     }
 
-    public function getId(): int{
-        return $this->id;
+    /**
+     * @return string
+     */
+    public function getNoSerie(): string
+    {
+        return $this->noSerie;
     }
+
+    /**
+     * @return string
+     */
+    public function getDateMiseEnCirculation(): string
+    {
+        return $this->dateMiseEnCirculation;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumModele(): int
+    {
+        return $this->numModele;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getClient(): Client
+    {
+        return $this->client;
+    }
+
 
     public function getNumberPlate(): string{
         return $this->numberPlate;
