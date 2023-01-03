6c119001c7ce92355e7fe388bdc8cac8c05ecfba
Léo Deroin <leo.deroin@etu.univ-grenoble-alpes.fr>
2023-01-03 09:26:00 +0100

    commit

--- assets/php/managers/TemplateManager.php
+++ assets/php/managers/TemplateManager.php
@@ -1,11 +1,96 @@
 <?php
 
+require_once "assets/php/managers/UserManager.php";
+
 class TemplateManager{
 
     public static function getDefaultNavBar(string $page){
-        switch($page){
-            case "accueil":
-                $navBar = '<nav class="nav-bar">
+        if($_SESSION["role"] == UserManager::EMPLOYE){
+            switch($page){
+                case "accueil":
+                    $navBar = '<nav class="nav-bar">
+                                <img src="../assets/img/logo.png" alt="logo">
+                                <ul>
+                                    <li class="hover"><a href="/accueil/employe" >Accueil</a></li>
+                                    <li><a href="#">Planning</a></li>
+                                    <li><a href="/stock">Stock</a></li>
+                                    <li><a href="#">Tarifs</a></li>
+                                    <li><a href="#">Clients</a></li>
+                                    <li><a href="/disconnect">Deconnexion</a></li>
+                                </ul>
+                            </nav>';
+                    break;
+                case "planning":
+                    $navBar = '<nav class="nav-bar">
+                                <img src="../assets/img/logo.png" alt="logo">
+                                <ul>
+                                    <li><a href="/accueil/employe" >Accueil</a></li>
+                                    <li class="hover"><a href="#">Planning</a></li>
+                                    <li><a href="/stock">Stock</a></li>
+                                    <li><a href="#">Tarifs</a></li>
+                                    <li><a href="#">Clients</a></li>
+                                    <li><a href="/disconnect">Deconnexion</a></li>
+                                </ul>
+                            </nav>';
+                    break;
+                case "rdv":
+                    $navBar = '<nav class="nav-bar">
+                                <img src="../assets/img/logo.png" alt="logo">
+                                <ul>
+                                    <li><a href="/accueil/employe">Accueil</a></li>
+                                    <li><a href="#">Planning</a></li>
+                                    <li><a href="/stock">Stock</a></li>
+                                    <li><a href="#">Tarifs</a></li>
+                                    <li><a href="#">Clients</a></li>
+                                    <li><a href="/disconnect">Deconnexion</a></li>
+                                </ul>
+                            </nav>';
+                    break;
+                case "stock":
+                    $navBar = '<nav class="nav-bar">
+                                <img src="../assets/img/logo.png" alt="logo">
+                                <ul>
+                                    <li><a href="/accueil/employe">Accueil</a></li>
+                                    <li><a href="#">Planning</a></li>
+                                    <li class="hover"><a href="/stock">Stock</a></li>
+                                    <li><a href="#">Tarifs</a></li>
+                                    <li><a href="#">Clients</a></li>
+                                    <li><a href="/disconnect">Deconnexion</a></li>
+                                </ul>
+                            </nav>';
+                    break;
+                case "tarifs":
+                    $navBar = '<nav class="nav-bar">
+                                <img src="../assets/img/logo.png" alt="logo">
+                                <ul>
+                                    <li><a href="/accueil/employe">Accueil</a></li>
+                                    <li><a href="#">Planning</a></li>
+                                    <li><a href="/stock">Stock</a></li>
+                                    <li class="hover"><a href="#">Tarifs</a></li>
+                                    <li><a href="#">Clients</a></li>
+                                    <li><a href="/disconnect">Deconnexion</a></li>
+                                </ul>
+                            </nav>';
+                    break;
+                case "clients":
+                    $navBar = '<nav class="nav-bar">
+                                <img src="../assets/img/logo.png" alt="logo">
+                                <ul>
+                                    <li><a href="/accueil/employe">Accueil</a></li>
+                                    <li><a href="#">Planning</a></li>
+                                    <li><a href="/stock">Stock</a></li>
+                                    <li><a href="#">Tarifs</a></li>
+                                    <li class="hover"><a href="#">Clients</a></li>
+                                    <li><a href="/disconnect">Deconnexion</a></li>
+                                </ul>
+                            </nav>';
+                    break;
+            }
+
+        }else if($_SESSION["role"] == UserManager::MANAGER){
+            switch($page){
+                case "accueil":
+                    $navBar = '<nav class="nav-bar">
                                 <img src="../assets/img/logo.png" alt="logo">
                                 <ul>
                                     <li class="hover"><a href="/accueil/employe" >Accueil</a></li>
@@ -17,9 +102,9 @@ class TemplateManager{
                                     <li><a href="/disconnect">Deconnexion</a></li>
                                 </ul>
                             </nav>';
-                break;
-            case "planning":
-                $navBar = '<nav class="nav-bar">
+                    break;
+                case "planning":
+                    $navBar = '<nav class="nav-bar">
                                 <img src="../assets/img/logo.png" alt="logo">
                                 <ul>
                                     <li><a href="/accueil/employe" >Accueil</a></li>
@@ -31,9 +116,9 @@ class TemplateManager{
                                     <li><a href="/disconnect">Deconnexion</a></li>
                                 </ul>
                             </nav>';
-                break;
-            case "rdv":
-                $navBar = '<nav class="nav-bar">
+                    break;
+                case "rdv":
+                    $navBar = '<nav class="nav-bar">
                                 <img src="../assets/img/logo.png" alt="logo">
                                 <ul>
                                     <li><a href="/accueil/employe">Accueil</a></li>
@@ -45,9 +130,9 @@ class TemplateManager{
                                     <li><a href="/disconnect">Deconnexion</a></li>
                                 </ul>
                             </nav>';
-                break;
-            case "stock":
-                $navBar = '<nav class="nav-bar">
+                    break;
+                case "stock":
+                    $navBar = '<nav class="nav-bar">
                                 <img src="../assets/img/logo.png" alt="logo">
                                 <ul>
                                     <li><a href="/accueil/employe">Accueil</a></li>
@@ -59,9 +144,9 @@ class TemplateManager{
                                     <li><a href="/disconnect">Deconnexion</a></li>
                                 </ul>
                             </nav>';
-                break;
-            case "tarifs":
-                $navBar = '<nav class="nav-bar">
+                    break;
+                case "tarifs":
+                    $navBar = '<nav class="nav-bar">
                                 <img src="../assets/img/logo.png" alt="logo">
                                 <ul>
                                     <li><a href="/accueil/employe">Accueil</a></li>
@@ -73,9 +158,9 @@ class TemplateManager{
                                     <li><a href="/disconnect">Deconnexion</a></li>
                                 </ul>
                             </nav>';
-                break;
-            case "clients":
-                $navBar = '<nav class="nav-bar">
+                    break;
+                case "clients":
+                    $navBar = '<nav class="nav-bar">
                                 <img src="../assets/img/logo.png" alt="logo">
                                 <ul>
                                     <li><a href="/accueil/employe">Accueil</a></li>
@@ -87,7 +172,9 @@ class TemplateManager{
                                     <li><a href="/disconnect">Deconnexion</a></li>
                                 </ul>
                             </nav>';
-                break;
+                    break;
+            }
+
         }
         echo $navBar;
     }
